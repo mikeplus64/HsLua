f193124b8853a64f2393b5ba4d9cd56660324404
Mike Ledger <eleventynine@gmail.com>
2012-12-28 22:22:41 +1100

    use Lua.Types

--- src/Lua.hs
+++ src/Lua.hs
@@ -53,7 +53,7 @@
 module Lua
 (
     -- * Basic Lua types
-    LuaState(..),
+    LuaState,
     LuaCFunction,
     LuaInteger,
     LuaNumber,
@@ -199,75 +199,7 @@ import qualified Data.ByteString    as B
 import Data.Word (Word8)
 import Data.Char (isLatin1)
 
--- | Wrapper for @lua_State *@. See @lua_State@ in Lua Reference Manual.
-newtype LuaState = LuaState (Ptr ())
-
--- | Wrapper for @lua_Alloc@. See @lua_Alloc@ in Lua Reference Manual.
-type LuaAlloc = Ptr () -> Ptr () -> CSize -> CSize -> IO (Ptr ())
-
--- | Wrapper for @lua_Reader@. See @lua_Reader@ in Lua Reference Manual.
-type LuaReader = Ptr () -> Ptr () -> Ptr CSize -> IO (Ptr CChar)
-
--- | Wrapper for @lua_Writer@. See @lua_Writer@ in Lua Reference Manual.
-type LuaWriter = LuaState -> Ptr CChar -> CSize -> Ptr () -> IO CInt
-
--- | Wrapper for @lua_CFunction@. See @lua_CFunction@ in Lua Reference Manual.
-type LuaCFunction = LuaState -> IO CInt
-
--- | Wrapper for @lua_Integer@. See @lua_Integer@ in Lua Reference Manual.
--- HsLua uses C @ptrdiff_t@ as @lua_Integer@.
-type LuaInteger = CPtrdiff
-
--- | Wrapper for @lua_Number@. See @lua_Number@ in Lua Reference Manual.
--- HsLua uses C @double@ as @lua_Integer@.
-type LuaNumber = CDouble
-
--- | Enumeration used as type tag. See @lua_type@ in Lua Reference Manual.
-data LTYPE = TNONE
-           | TNIL
-           | TBOOLEAN
-           | TLIGHTUSERDATA
-           | TNUMBER
-           | TSTRING
-           | TTABLE
-           | TFUNCTION
-           | TUSERDATA
-           | TTHREAD
-           deriving (Eq,Show,Ord)
-
-instance Enum LTYPE where
-    fromEnum TNONE          = -1
-    fromEnum TNIL           = 0
-    fromEnum TBOOLEAN       = 1
-    fromEnum TLIGHTUSERDATA = 2
-    fromEnum TNUMBER        = 3
-    fromEnum TSTRING        = 4
-    fromEnum TTABLE         = 5
-    fromEnum TFUNCTION      = 6
-    fromEnum TUSERDATA      = 7
-    fromEnum TTHREAD        = 8
-    toEnum (-1)             = TNONE
-    toEnum 0                = TNIL
-    toEnum 1                = TBOOLEAN
-    toEnum 2                = TLIGHTUSERDATA
-    toEnum 3                = TNUMBER
-    toEnum 4                = TSTRING
-    toEnum 5                = TTABLE
-    toEnum 6                = TFUNCTION
-    toEnum 7                = TUSERDATA
-    toEnum 8                = TTHREAD
-    toEnum n                = error $ "Cannot convert (" ++ show n ++ ") to LTYPE"
-
--- | Enumeration used by @gc@ function.
-data GCCONTROL  = GCSTOP
-                | GCRESTART
-                | GCCOLLECT
-                | GCCOUNT
-                | GCCOUNTB
-                | GCSTEP
-                | GCSETPAUSE
-                | GCSETSTEPMUL
-                deriving (Eq,Ord,Show,Enum)
+import Lua.Types
 
 -- | See @LUA_MULTRET@ in Lua Reference Manual.
 multret :: Int
@@ -821,25 +753,6 @@ argerror l n msg = B.useAsCString msg $ \msg -> do
     freeHaskellFunPtr f
     -- here we should have error message string on top of the stack
     return (-1)
-
-
-
--- | A value that can be pushed and poped from the Lua stack.
--- All instances are natural, except following:
---
---  * @LuaState@ push ignores its argument, pushes current state
---
---  * @()@ push ignores its argument, just pushes nil
---
---  * @Ptr ()@ pushes light user data, peek checks for lightuserdata or userdata
-class StackValue a where
-    -- | Pushes a value onto Lua stack, casting it into meaningfully nearest Lua type.
-    push :: LuaState -> a -> IO ()
-    -- | Check if at index @n@ there is a convertible Lua value and if so return it
-    -- wrapped in @Just@. Return @Nothing@ otherwise.
-    peek :: LuaState -> Int -> IO (Maybe a)
-    -- | Lua type id code of the vaule expected. Parameter is unused.
-    valuetype :: a -> LTYPE
 
 maybepeek :: l -> n -> (l -> n -> IO Bool) -> (l -> n -> IO r) -> IO (Maybe r)
 maybepeek l n test peek = do
